a3bd156523b2ad91d898787ba81664532d175b46
Jonathan Florian ROUQUETTE <rouquettej@gmail.com>
2019-01-22 16:16:13 +0100

    Réorganisation et finalisation

--- 7/Program.cs
+++ 7/Program.cs
@@ -19,33 +19,4 @@ namespace _7
             Console.ReadKey();
         }
     }
-
-    class Rectangle
-    {
-        public decimal Width { get; set; }
-        public decimal Length { get; set; }
-
-        public decimal Perimetter
-        {
-            get { return (2 * (Width + Length)); }
-        }
-        public decimal Area
-        {
-            get { return (Width * Length); }
-        }
-
-        public string Affichage
-        {
-            get
-            {
-                return $"Le rectangle Lx {Length} lx {Width} à un périmètre de : {Perimetter}, et une aire de : {Area}.";
-            }
-        }
-
-        public Rectangle(decimal pWidth, decimal pLength)
-        {
-            Length = pLength;
-            Width = pWidth;
-        }
-    }
 }
---
+++ 7/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7
+{
+    class Rectangle
+    {
+        public decimal Width { get; set; }
+        public decimal Length { get; set; }
+
+        public decimal Perimetter
+        {
+            get { return (2 * (Width + Length)); }
+        }
+        public decimal Area
+        {
+            get { return (Width * Length); }
+        }
+
+        public string Affichage
+        {
+            get
+            {
+                return $"Le rectangle Lx {Length} lx {Width} à un périmètre de : {Perimetter}, et une aire de : {Area}.";
+            }
+        }
+
+        public Rectangle(decimal pWidth, decimal pLength)
+        {
+            Length = pLength;
+            Width = pWidth;
+        }
+    }
+}
